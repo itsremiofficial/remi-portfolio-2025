859de3a526fb7e9d16dc8cfb4c9a3b6db3a090d8
REMI <84964174+itsremiofficial@users.noreply.github.com>
2025-08-23 17:33:52 +0500

    up

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -3,15 +3,15 @@ import ModernArrow from "../components/ModernArrow";
 const Hero = () => {
   return (
     <section
-      className="w-full min-h-[calc(100vh-6rem)] flex items-center justify-center bg-zinc-800"
+      className="w-full min-h-[calc(100vh-6rem)] flex items-center justify-center"
       id="home"
     >
       <div className="text-center space-y-3">
-        <h1 className="hero-heading font-nippo font-bold text-white flex items-center justify-center gap-12">
+        <h1 className="hero-heading font-nippo flex items-center justify-center gap-12 font-medium">
           Designer <ModernArrow className="w-14 h-max" /> Developer
         </h1>
-        <p className="mt-4 text-[10vw] text-gray-300 text-wrap ">
-          I Turn Imagination <br /> Into Interactive Digital Experiences
+        <p className="mt-4 text-[10vw] text-wrap">
+          I Turn Imagination <br /> Into Interactive Digital <br /> Experiences
         </p>
       </div>
     </section>
